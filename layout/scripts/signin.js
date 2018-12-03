0e1f031533e0349b213b224a007b3f9d20fd9077
KM Bhat <kmanasvibhat@gmail.com>
2018-12-03 16:46:05 +0530

    Improved UI

--- layout/scripts/signin.js
+++ layout/scripts/signin.js
@@ -1,3 +1,5 @@
+db = "http://127.0.0.1:5000/"
+
 var vContinue = document.getElementById("continue"),
     vLogin = document.getElementById("login");
 
@@ -16,3 +18,23 @@ function PrefixedEvent(element, type, callback) {
 PrefixedEvent(vLogin, "AnimationEnd", function () {
   document.body.className = '';
 });
+
+//login functionality
+function login(uname, password) {
+  s = new FormData();
+  s.append('username', name);
+  s.append('password', password)
+  var g = $.ajax({
+      url: db + '/new', 
+      type: 'POST',
+      processData : false,
+      contentType : false, 
+      crossDomain: true,
+      data: s
+
+  }).done(function(data) {
+      if (data.login == 'true') window.location = db+'officer/'+data.uid.toString();
+      else 
+});
+}
+
