e9bd11f64d428b08e82137770d799fd6d39613a0
Lohith N V <n.vlohithnv@gmail.com>
2018-12-07 18:19:58 +0530

    changes to frontend

--- layout/scripts/register.js
+++ layout/scripts/register.js
@@ -2,11 +2,12 @@ fname = document.getElementById('fname');
 lname = document.getElementById('lname');
 OID = document.getElementById('OID');
 title = document.getElementById('title');
-status = document.getElementById('status');
 phone = document.getElementById('phone');
 salary = document.getElementById('salary');
 uname = document.getElementById('uname');
 password = document.getElementById('password');
+
+db = 'http://127.0.0.1:5000/'
 
 //login functionality
 function register() {
@@ -15,14 +16,13 @@ function register() {
     s.append('lname', lname.value);
     s.append('OID', OID.value);
     s.append('title', title.value);
-    s.append('status', status.value);
     s.append('phone', phone.value);
     s.append('salary', salary.value);
     s.append('uname', uname.value);
     s.append('password', password.value);
     
     var g = $.ajax({
-        url: db + 'login', 
+        url: db + 'register', 
         type: 'POST',
         processData : false,
         contentType : false, 
@@ -30,7 +30,7 @@ function register() {
         data: s
   
     }).done(function(data) {
-        window.location = db+'login.html';
+        // window.location = db+'login.html';
   });
   }
   function test () {
